79bf40ac484f70009f63939a67cd86211b2cf99d
Maylilll <642722474@qq.com>
2023-04-22 15:16:45 +0800

    fix:Schedule logging

--- src/EasySchedule/EasySchedule.py
+++ src/EasySchedule/EasySchedule.py
@@ -11,67 +11,86 @@ import functools
 import logging
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../'))
 
-def catch_exceptions_decorator(job_func):
+LOG_PATH = "test.log"
+def catch_exceptions_decorator(job_func, logger):
     @functools.wraps(job_func)
     def wrapper(*args, **kwargs):
         try:
             return job_func(*args, **kwargs)
         except Exception as e:
             import traceback
-            print(traceback.format_exc())
+            logger.error(traceback.format_exc())
     return wrapper
 
-logger = logging.getLogger(__name__)
-logger.setLevel(level = logging.INFO)
-handler = logging.FileHandler("log.txt")
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.info("Do something")
+def logger():
+    logger = None
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level = logging.INFO)
+    if not logger.handlers:
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.DEBUG)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        sh.setFormatter(formatter)
+        handler = logging.FileHandler(LOG_PATH)
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.addHandler(sh)
+    return logger
+
 database_porxy = pw.Proxy()
 class EasySchedule:
     peewee = pw
     schedule = schedule
-    logger = logger
     CONFIG_FILE_PATH = 'config.yml'
     SCHEDULES_PATH = 'schedules'
     MODELS_PATH = 'models'
+    LOG_PATH = 'run.log'
+    logger = None
     database = database_porxy
 
-    def __init__(self, config_path, schedules_path, models_path):
+    def __init__(self, config_path, schedules_path, models_path, log_path):
         self.CONFIG_FILE_PATH = config_path
         self.SCHEDULES_PATH = schedules_path
         self.MODELS_PATH = models_path
+        self.LOG_PATH = log_path
+        global LOG_PATH
+        LOG_PATH = self.LOG_PATH
+        self.logger = logger()
         self.init_peewee()
         self.init_terminal()
         self.init_schedule()
         pass
 
-    def exec_do(self, class_):
+    def exec_do(self, name, class_):
         if(hasattr(class_,'cron') and class_.cron):
+            # print(name)
             trigger = self.schedule_trigger(class_.cron)
-            newFunc = catch_exceptions_decorator(class_().trigger)
+            class_.logger = self.logger
+            c = class_()
+            newFunc = catch_exceptions_decorator(c.trigger, self.logger)
             trigger.do(newFunc)
         else:
+            class_.logger = self.logger
             c = class_()
             try:
                 c.trigger()
             except Exception as e:
                 import traceback
-                print(traceback.format_exc())
+                class_.logger.error(traceback.format_exc())
         pass
 
     # run_pending of schedule
     def run_pending(self):
-        print("EasySchedule Framework is run_pending...")
+        self.logger.info("EasySchedule Framework is run_pending...")
         while True:
             schedule.run_pending() # 运行所有可运行的任务
             time.sleep(1)
 
     #init_terminal
     def init_terminal(self):
-        print(
+        self.logger.info(
             """
                             
                    ____                ____    __          __     __   
@@ -134,7 +153,7 @@ class EasySchedule:
             members = inspect.getmembers(module["model"], inspect.isclass)
             for(name, class_) in members:
                 if(str(class_).find('schedules') != -1):
-                    self.exec_do(class_)
+                    self.exec_do(name, class_)
         pass
 
     def get_all_modules(self, dir):
--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from EasySchedule import EasySchedule
 CONFIG_FILE_PATH = "config.yml"
 SCHEDULES_PATH = "schedules"
 MODELS_PATH = "models"
+LOG_PATH = "run.log"
 
-easySchedule = EasySchedule(CONFIG_FILE_PATH, SCHEDULES_PATH, MODELS_PATH)
+easySchedule = EasySchedule(CONFIG_FILE_PATH, SCHEDULES_PATH, MODELS_PATH, LOG_PATH)
 easySchedule.run_pending()
